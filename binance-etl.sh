9f57994384efe0412c80caacdc2756eadb23c67f
georgy <georgy@analytics-abc.xyz>
2024-04-20 16:11:56 +0300

    vars

--- binance-etl.sh
+++ binance-etl.sh
@@ -2,7 +2,7 @@
 
 cd $ETL_DIR &&
     printf "Starting ETL process  $(date) \n\n" >> binance.log &&
-    bb ./binance-download.clj >> binance.log &&
-    bb ./binance-edn-parser.clj >> binance.log &&
-    bb ./binance-to-db.clj >> binance.log &&
+    bb $ETL_DIR/binance-download.clj >> binance.log &&
+    bb $ETL_DIR/binance-edn-parser.clj >> binance.log &&
+    bb $ETL_DIR/binance-to-db.clj >> binance.log &&
     printf " \n\n ETL process ended on  $(date) \n\n" >> binance.log
